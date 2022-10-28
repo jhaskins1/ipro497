60f01cc0152888fb088c4e0dc20e9be4d9a2226c
isheikh8492 <isheikh8492@gmail.com>
2022-10-28 09:27:00 -0500

    login signup page set

--- main/app/views.py
+++ main/app/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import SignUpForm
 
 def index(request):
     return HttpResponse("Hello world!")
 
 def signup(request):
-    return render(request, 'registration/signup.html')
+    form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
