d928edbda64c333b000a7d39ee5b1ec2aac1f4ed
isheikh8492 <isheikh8492@gmail.com>
2022-10-28 13:27:50 -0500

    added navbar on home signup and login

--- main/app/views.py
+++ main/app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from .forms import SignUpForm
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'app/home.html')
 
 def signup(request):
     form = SignUpForm()
