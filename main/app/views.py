6f1935e4003afb805e18509c3308c4410648741f
isheikh8492 <isheikh8492@gmail.com>
2022-10-31 11:08:58 -0500

    added sample movie data on home page

--- main/app/views.py
+++ main/app/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 from .forms import SignUpForm
+import requests
 
 def index(request):
-    return render(request, 'app/home.html')
+    api_key = "864e3baa97800ac0d0036fd1d008ca85"
+    movie_list = []
+    for id in range(550, 557):
+        url = "https://api.themoviedb.org/3/movie/" + str(id) + "?api_key=" + api_key + "&language=en-US"
+        response = requests.get(url)
+        movie_list.append(response.json())
+    return render(request, 'app/home.html', {'movie_list': movie_list})
 
 def signup(request):
     form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
+
+# def recommend(request):
+#     api_key = "864e3baa97800ac0d0036fd1d008ca85"
+#     movie_list = []
+#     for id in range(550, 557):
+#         url = "https://api.themoviedb.org/3/movie/" + id + "?api_key=" + api_key + "&language=en-US"
+#         response = requests.get(url)
+#         movie_list.append(response.json())
+
+    
+
+
 
