3973d68f673ddcb0578717c7a4e4cb70d3d29ca0
isheikh8492 <isheikh8492@gmail.com>
2022-11-01 19:51:30 -0500

    added user registration feature

--- main/app/forms.py
+++ main/app/forms.py
@@ -1,14 +1,13 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import fields
 from django.forms.widgets import EmailInput
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    f_name = forms.CharField(label="First Name", max_length=30, required=True)
-    l_name = forms.CharField(label="Last Name", max_length=30, required=True)
+    first_name = forms.CharField(label="First Name", max_length=30, required=True)
+    last_name = forms.CharField(label="Last Name", max_length=30, required=True)
     email = forms.EmailField(label="Email Address", widget=EmailInput, max_length=254)
 
     class Meta:
         model = User
-        fields = ("f_name", "l_name", "username", "email",)
+        fields = ("first_name", "last_name", "username", "email", 'password1', 'password2')
--- main/app/views.py
+++ main/app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from .forms import SignUpForm
 import requests
 
@@ -12,7 +13,20 @@ def index(request):
     return render(request, 'app/home.html', {'movie_list': movie_list})
 
 def signup(request):
-    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    
     return render(request, 'registration/signup.html', {'form': form})
 
 # def recommend(request):
