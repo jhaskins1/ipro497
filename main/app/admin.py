5c13420d3cd8a46d2e99a189480bb09b5c10342f
isheikh8492 <isheikh8492@gmail.com>
2022-11-08 19:52:26 -0600

    comment codebase

--- main/app/admin.py
+++ main/app/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from .models import Movie, Genre, Cast, Language, StreamingOption
 # Register your models here.
 
+# Once you create the models and migrate the model specifications to the database, you will need to register
+# the model into the admin interface for verification and total control. Admin section is something that we won't
+# use, but it will serve as verification that data is being recorded into the database.
+
+
 admin.site.register(Movie)
 admin.site.register(Genre)
 admin.site.register(Cast)
